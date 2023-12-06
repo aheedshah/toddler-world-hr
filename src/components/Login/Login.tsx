45a89d81e8770ff30c08a1726ada83b168b1eacd
Aheed Shah <aheed@duck.com>
2023-12-06 13:33:57 +0000

    feat: add cypress tests

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -4,51 +4,51 @@ import './Login.scss'
 import { BASE_URL } from '../../apiConfig'
 
 const Login: React.FC = () => {
-  const navigate = useNavigate()
-  const [username, setUsername] = useState<String>('')
-  const [password, setPassword] = useState<String>('')
-  const [error, setError] = useState<String>('')
+    const navigate = useNavigate()
+	const [username, setUsername] = useState<String>('')
+	const [password, setPassword] = useState<String>('')
+	const [error, setError] = useState<String>('')
 
-  const handleLogin = async (e: FormEvent) => {
-    e.preventDefault()
+	const handleLogin = async (e: FormEvent) => {
+		e.preventDefault()
 
-    const response = await fetch(`${BASE_URL}/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ username, password })
-    })
+		const response = await fetch(`${BASE_URL}/login`, {
+			method: 'POST',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ username, password })
+		});
 
-    if (response.ok) {
-      const data = await response.json()
-      navigate(data.redirectUrl)
-    } else {
-      setError('Wrong Email/Password')
-    }
-  }
+		if (response.ok) {
+			const data = await response.json()
+			navigate(data.redirectUrl)
+		} else {
+			setError('Wrong Email/Password')
+		}
+	}
 
-  return (
-    <div className='login-container'>
-      <form onSubmit={handleLogin}>
-        <img src='/images/logo.webp' alt='Toddler World Logo' /> <br />
-        <input
-          type='email'
-          placeholder='Email'
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        /> <br />
-        <input
-          type='password'
-          placeholder='Password'
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        /> <br />
-        <button type='submit'>Login</button>
-        {error && <p>{error}</p>}
-      </form>
-    </div>
-  )
+	return (
+		<div className='login-container'>
+		<form onSubmit={handleLogin}>
+		    <img src='/images/logo.webp' alt='Toddler World Logo' /> <br />
+		    <input
+		        type='email'
+		        placeholder='Email'
+		        onChange={(e) => setUsername(e.target.value)}
+		        required
+		    /> <br />
+		    <input
+		        type='password'
+		        placeholder='Password'
+		        onChange={(e) => setPassword(e.target.value)}
+		        required
+		    /> <br />
+		    <button type='submit'>Login</button>
+		    {error && <p>{error}</p>}
+		</form>
+		</div>
+	)
 }
 
 export default Login
